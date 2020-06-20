5450293cad733a6141bcc033d5ffd77d0475970e
Antoine Gordon-Alvarez <antoinegordon-alvarez@MacBook-Pro.local>
2020-06-19 21:54:28 -0400

    fixinf npm start to run

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const passport = require('passport');
 
 require('dotenv').config();
 
-app.use(express.static(path.join(__dirname, 'client' ,'build')));
+app.use(express.static(path.join(__dirname, 'client' ,'public')));
 app.use(cors());
  
 // Bodyparser middleware
