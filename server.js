ee3bc1026a602909c67a8aaf82f7805cf23bd8bd
Antoine Gordon-Alvarez <antoinegordon-alvarez@MacBook-Pro.local>
2020-06-19 21:47:54 -0400

    fixinf npm start to run

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const passport = require('passport');
 
 require('dotenv').config();
 
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client' ,'build')));
 app.use(cors());
  
 // Bodyparser middleware
@@ -39,7 +39,7 @@ app.use('/api/weather', weather)
 
 app.get('/', (req, res) => {
   
-  res.sendFile(path.join('client' ,'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client' ,'build', 'index.html'))
 });
 
 mongoose.connect(
